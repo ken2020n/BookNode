c4e66c7c3b553eddbcfb1f01a0dc2c97a400b751
ken <ken2020n@users.noreply.github.com>
2022-07-20 19:41:57 +0900

    add knex support and connection test

---
+++ db/migrations/20220720102331_create_user_table.js
@@ -0,0 +1,24 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable("user", function (table) {
+    table.increments("id").primary();
+    table
+      .string("email", 64)
+      .unique()
+      .notNullable()
+      .index();
+    table.string("name", 32).notNullable;
+    table.string("password", 64).notNullable;
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable("user"); 
+};
--- src/server.js
+++ src/server.js
@@ -1,9 +1,15 @@
 const express = require("express");
+require("dotenv").config();
 
 const setupServer = () => {
   const app = express();
   app.use(express.json());
   app.get("/test", (req, res) => {
+    res.send("ok").status(200);
+  });
+
+  app.get("/dbtest", (req, res) => {
+    console.log("DB_NAME: ", process.env.DB_NAME);
     res.send("ok").status(200);
   });
 
